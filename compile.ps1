e3cda6e027178380808a0c5410a9200ce1a52b67
Igneaalis <Nostaleal.ru@yandex.ru>
2021-01-12 03:03:09 +0400

    Tasks update. config.ini added

--- compile.ps1
+++ compile.ps1
@@ -1,7 +1,19 @@
-$SourceFolder = "./source/*"
-$OutputFolder = "./output"
-$OutputFile = "./output/raw_war3map.j"
+param (
+    [switch]$debug = $false,
+    [switch]$worldeditor = $false
+)
+
+$SourceFolder = "$pwd/source/*"
+$OutputFolder = "$pwd/output"
+$OutputFile = "$pwd/output/raw_war3map.j"
 $SourceFiles = Get-ChildItem -Path $SourceFolder -Recurse -Include *.j -Exclude Blizzard.j, common.j, war3map.j, base.j
+$JassHelperArgs = "$pwd\tools\common.j", "$pwd\tools\Blizzard.j", "$pwd\output\raw_war3map.j", "$pwd\output\MIX.w3x"
+
+$ConfigData = Get-Content ".\config.ini"
+$ConfigData = $ConfigData -replace "\\+", "\\"
+$ConfigData = $ConfigData -replace "\[\S*\]", "" -match "\S*=[\S ]*" | Out-String
+
+$Config = ConvertFrom-StringData -StringData $ConfigData
 
 New-Item -Path $OutputFolder -ItemType Directory -Force > $null
 New-Item -Path $OutputFolder -Name "raw_war3map.j" -ItemType File -Force > $null
@@ -10,6 +22,21 @@ foreach ($File in $SourceFiles) {
     Get-Content -Path $File | Add-Content $OutputFile
 }
 
-Get-Content -Path "./source/main/base.j" | Add-Content $OutputFile
+Get-Content -Path "$pwd/source/main/base.j" | Add-Content $OutputFile
 
 Write-Output "Compiled successfully!"
+
+if ($Config.WarcraftPath -match "\b\w[:]((\\|\/)(\w|\d|[ ])*)*\b") {
+    if ($debug) {
+        $JassHelperArgs = "--debug" + " " + $JassHelperArgs
+    }
+    Start-Process -FilePath "$pwd\tools\JassHelper\clijasshelper.exe" -ArgumentList $JassHelperArgs -NoNewWindow -Wait
+    if ($worldeditor) {
+        Start-Process -FilePath $Config.WorldEditorPath -ArgumentList "-launch", "-loadfile", "$pwd\output\MIX.w3x"
+    } else {
+        Start-Process -FilePath $Config.WarcraftPath -ArgumentList "-launch", "-loadfile", "$pwd\output\MIX.w3x", "-windowmode windowedfullscreen", "-testmapprofile Player", "-fixedseed 1"
+    }
+    Write-Output "`n`nLaunched successfully!"
+} else {
+    Write-Output "EXCEPTION: `"Warcraft III.exe`" is not found!`nPlease check:`n`t-> .\launch.ini`n`t`t-> WarcraftPath"
+}
